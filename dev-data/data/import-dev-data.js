f5ff1e2d7d23677f568543d9bd3d433d1fad198c
code-art4 <ojotriumph4@gmail.com>
2022-03-26 07:32:58 +0100

    first commit

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successfull'));
+
+//READ JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//IMPORT Data into Database
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Database successfully loaded')
+    } catch (err) {
+        console.log(err)
+    }
+    process.exit();
+}
+
+
+//DELETE ALL DATA FROM DB
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Database successfully deleeted');
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
+};
+
+
+if(process.argv[2] === "--import"){
+    importData();
+}else if(process.argv[2] === "--delete"){
+    deleteData()
+}
+console.log(process.argv)
